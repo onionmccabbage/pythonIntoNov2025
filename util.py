ae855a8cca5ae1132c303d438eb93fd1fa560a14
onionmccabbage <onionmccabbage@live.com>
2025-11-24 16:28:38 +0000

    working with __name__

--- util.py
+++ util.py
@@ -14,8 +14,15 @@ def checkNumeric(n=0): # we expect a single positional argument. We may choose t
     #     return 0
     # alternative syntax  [value_1] if [expression] else [value_2]
     return n if valid else 0
-    
+
+# what does Python call this module???
+# dunder means leading and trailngi double underscore
+# When run directly the module is ALWAYS named __main__
+# when imported, python ALWAYS assignes __name__ to the module name ('util' in this case)
+print(__name__) # NB anything with dunder belongs to Python
+
 if __name__ == '__main__':
+    ''' When the mdule is run directly, the following exercise code will run'''
     print( checkNumeric(33) )
     print( checkNumeric(-33) )
     print( checkNumeric(3.3) )
