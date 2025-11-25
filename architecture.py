966af8c4e8ba2fc95b614397b9c2473eb3d6a649
onionmccabbage <onionmccabbage@live.com>
2025-11-25 10:00:14 +0000

    importing from relative locations

--- architecture.py
+++ architecture.py
@@ -3,9 +3,13 @@ import datetime # we have access to everything in the datetime library
 # alternatively we may just import the bits we need
 from datetime import datetime # we now have just teh datetime feature from the datetime library
 # we can import from our own modules
-import util # we have it all
+import util as util # we have it all
 # or
 from util import checkNumeric
+# modules within folders are accessed like this
+from my_assets.my_code import herewego
+# NB the Python interpreter always begins looking from the root
+# where the root is the place we ran Python
 
 # there is never a problem to including functions within a main module
 def today():
@@ -22,6 +26,7 @@ def main():
     # make use of our imported util
     print( util.checkNumeric(42) )
     print( checkNumeric('42') )
+    herewego() # call our imported function
 
 
 if __name__ == '__main__':
---
+++ my_assets/my_code.py
@@ -0,0 +1,3 @@
+#this will be imported elsewhere
+def herewego():
+    print(f'This module is called {__name__}')
