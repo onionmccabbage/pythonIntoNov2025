54ade9291e035e1717e92cb959dc5e3f1244d7ef
onionmccabbage <onionmccabbage@live.com>
2025-11-25 16:25:22 +0000

    using file .write()

---
+++ fifo/write_file.py
@@ -0,0 +1,17 @@
+# Sometimes we need more control when persisting in a text file
+
+def writeTextFile(s):
+    '''Commit the string 's' to a text file using write()'''
+    try:
+        with open('my_log.txt', 'at') as fout:
+            fout.write() # write() sill ommit the entire string uing the file access object
+            # no need to close()
+    except FileNotFoundError as err:
+        print(f'No such file {err}')
+    except Exception as err:
+        print(f'General Error: {err}')
+
+
+if __name__ == '__main__':
+    s = 'writing to a file'
+    writeTextFile(s)
