4eb872949be2fb147dcae4bc461f7438ed71b162
onionmccabbage <onionmccabbage@live.com>
2025-11-25 16:29:02 +0000

    working with file access

--- fifo/write_file.py
+++ fifo/write_file.py
@@ -3,8 +3,9 @@
 def writeTextFile(s):
     '''Commit the string 's' to a text file using write()'''
     try:
-        with open('my_log.txt', 'at') as fout:
-            fout.write(s) # write() sill ommit the entire string uing the file access object
+        with open('my_log.txt', 'at') as fout: # we could use 'xt' for exclusive file access
+            # Careful: by default write() does not add a new line character
+            fout.write(f'{s}\n') # write() sill ommit the entire string uing the file access object
             # no need to close()
     except FileNotFoundError as err:
         print(f'No such file {err}')
