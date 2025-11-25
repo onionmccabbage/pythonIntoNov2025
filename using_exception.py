334e948128bf267774e5f193863dfaa8294a0b09
onionmccabbage <onionmccabbage@live.com>
2025-11-25 10:57:35 +0000

    raising our own exceptions

--- using_exception.py
+++ using_exception.py
@@ -9,14 +9,21 @@ def myFn():
     # we need to make sure the string is numeric
     try:
         n = int(float(response))
+        # if there was no exception our code reaches here
+        if n not in range(0, 101):
+            raise TypeError('Number must be 0-100')
     except ValueError as err: # we may choose to handle specific kinds of exception
         print(f'There is a problem {err}')
+    except TypeError as err:
+        print(err)
     except Exception as err:
         print(f'A more serious problem occured {err}')
     finally:
         print('The finally block will always run, whether or not we had an exception')
 
+
+
 if __name__ == '__main__':
     '''this is where we exercise the code'''
     myFn()
-    myFn()
+    myFn() # even if there was an exception we can continue to run our code
