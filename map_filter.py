18f8bbe43406064a33ad6871ac1bf2e1674ae67e
onionmccabbage <onionmccabbage@live.com>
2025-11-26 15:42:42 +0000

    using map

---
+++ map_filter.py
@@ -0,0 +1,24 @@
+# map lets us apply a function to every value in a collection
+# filter lets us compare every value in a colection using a function
+
+def makeSquare(n):
+    '''return the square of the given value'''
+    return n**2
+
+def checkEven(n):
+    '''check to see if the given value is an even integer'''
+    isInt = False
+    if type(n)==int and (n/2 == int(n/2)):
+        isInt=True
+    return isInt
+
+def main():
+    '''It is common practice to have a function called main'''
+    # use map
+    s = map(makeSquare(), range(1,11)) # NB any collection will do - a range, a generator, a list a tuple...
+    print( s )
+
+
+if __name__ == '__main__':
+    '''exercise the code'''
+    main()
