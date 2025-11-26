b58d6b1e2ccb3f628ea969e38adcc45bc02c68c6
onionmccabbage <onionmccabbage@live.com>
2025-11-26 15:35:31 +0000

    using an endless custom generator

--- custom_gen.py
+++ custom_gen.py
@@ -1,13 +1,14 @@
 from datetime import datetime
+import time
 # Range and generators are great, but we can build our own too
 
 # simple range
 r = range(0,10)
 
 # a simple generator expression
-temperature = (f'{i}C' for i in range(-70, 91))
-for _ in temperature:
-    print(_, end='\t')
+# temperature = (f'{i}C' for i in range(-70, 91))
+# for _ in temperature:
+#     print(_, end='\t')
 
 # we may need to create a dat-time stamp, maybe to use when writing a log file
 def makeDT():
@@ -23,4 +24,14 @@ def makeDT():
 if __name__ == '__main__':
     # we need an instance of our generator
     dt = makeDT()
-    print(type(dt))
+    print(type(dt)) # we have a generator object
+    # we can use our genertor like this
+    t1 = dt.__next__() # this will yield the next available value from the generator
+    print(t1)
+    time.sleep(3) # about three seconds
+    t2 = dt.__next__()
+    print(t2)
+    # we may iterate but best if the genertor has a limit!!!
+
+
+
