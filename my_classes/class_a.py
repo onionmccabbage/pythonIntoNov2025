6444caebc2e46f7e22c1b73523db98157d613221
onionmccabbage <onionmccabbage@live.com>
2025-11-26 10:41:24 +0000

    a sample class with decorated properties

--- my_classes/class_a.py
+++ my_classes/class_a.py
@@ -5,10 +5,26 @@ class Person: # by convention we use initial cap for class names
     # remember - anything with double underscores fore and aft is part of python
     def __init__(self, n, a): # __init__ will run once every time we create an instance
         self.n = n # self is a way to refer to the created instance
-        self.a = a
+        self.a = a # self.a refers to the get/set methods for 'a'
+        self.checkAge(a)
+    # we often write methods of the class, including property validation methods
+    @property # here we decorate our function as a property
+    def a(self): # getter or accessor method. This function behaves as a property
+        '''this is the property getter method for the value 'a' '''
+        return self.__a
+    # Python never allows funtions to share the same name, except with get/set decorators
+    @a.setter # this setter will be invoked whenever we try to mutate the value of 'a'
+    def a(self, new_a): # setter or mutator method
+        '''this is the property setter method for the value 'a' '''
+        if type(new_a)==int and new_a>0:
+            self.__a = new_a # __a is called 'name mangling'
 
 
 if __name__ == '__main__':
     '''exercise our code'''
     c = Person('Olga', 32) # we now have an instance of our class (the __init__ runs once)
     f = Person('Floella', 72) # we now have an instance of our class (the __init__ runs once)
+    # by default class properties are mutable
+    c.a = [42, 0, True, None]
+    # we may see the values in our class instances
+    print(f'Name: {c.n} Age: {c.a}')
